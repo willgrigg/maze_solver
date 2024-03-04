6258642238331320825cac73114c3ae2876dd1b9
willgrigg <willwgrigg@gmail.com>
2024-03-04 08:39:06 -0700

    copy pasted from boot, should change

--- main.py
+++ main.py
@@ -1,55 +1,101 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("Maze")
-        self.canvas = Canvas(self.root, background="white", width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.is_running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-    
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-    
-    def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
-            self.redraw()
-    
-    def close(self):
-        self.is_running = False
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
 
-    def draw_line(self, fill_color, line) -> None:
-        line.draw(fill_color, self.canvas)
-        
-    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+    def close(self):
+        self.__running = False
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Line:
-    def __init__(self, point1 : Point, point2 : Point) -> None:
-        self.point1 = point1
-        self.point2 = point2
 
-    def draw(self, fill_color, canvas : Canvas) -> None:
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
         canvas.create_line(
-            self.point1.x, self.point1.y,
-            self.point2.x, self.point2.y, 
-            fill=fill_color, width=2
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
         canvas.pack(fill=BOTH, expand=1)
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
 
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
 
 def main():
     window = Window(800, 600)
-    point1 = Point(1, 1)
-    point2 = Point(100, 800)
-    line1 = Line(point1, point2)
-    window.draw_line("red", line1)
+
+    c = Cell(window)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(window)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(window)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(window)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     window.wait_for_close()
 
 main()
